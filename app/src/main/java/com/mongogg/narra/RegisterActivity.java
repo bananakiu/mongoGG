8323063a02c08e3e7350b24d955ed1ad526461bb
eddrichang <eddrichang@gmail.com>
2018-01-28 04:57:19 +0800

    Resolve conflicts

--- app/src/main/java/com/mongogg/narra/RegisterActivity.java
+++ app/src/main/java/com/mongogg/narra/RegisterActivity.java
@@ -2,6 +2,9 @@ package com.mongogg.narra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View wiew){
+                EditText editusername = findViewById(R.id.)
+
+
+            }
+        });
+
     }
 }
